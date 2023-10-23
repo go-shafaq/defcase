0f9b514d00908bcb36c1c856857b6978b4ba47b5
sherlock <abdullayev.13.dev@gmail.com>
2023-10-23 21:12:13 +0500

    .

---
+++ defcase.go
@@ -0,0 +1,27 @@
+package defcase
+
+import (
+	"github.com/go-shafaq/defcase/internal"
+)
+
+var tagNode *internal.PkgNode = internal.NewPkgNode()
+
+func DefCase(tag, pkgPath string, c Case) {
+	tagNode.SetCase(tag, pkgPath, c)
+}
+
+func ToDefCase(tag, pkgPath, name string) string {
+	c := tagNode.GetCase(tag, pkgPath)
+
+	return Convert(name, c)
+}
+
+func DefCaseFn(tag string) func(pkgPath, name string) string {
+	pkgNode := tagNode.GetOrSetSubNode(tag)
+
+	return func(pkgPath, name string) string {
+		c := pkgNode.GetCaseByPkg(pkgPath)
+
+		return Convert(name, c)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/go-shafaq/defcase
+
+go 1.20
+
+require github.com/iancoleman/strcase v0.2.0
